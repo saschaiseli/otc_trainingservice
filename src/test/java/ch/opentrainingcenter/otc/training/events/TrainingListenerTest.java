f0782b996041e9171bf1f9f8c022551cdae8b520
sascha iseli <sascha.iseli@gmx.ch>
2018-11-15 23:25:10 +0100

    add arquillian integration test

--- src/test/java/ch/opentrainingcenter/otc/training/events/TrainingListenerTest.java
+++ src/test/java/ch/opentrainingcenter/otc/training/events/TrainingListenerTest.java
@@ -1,11 +1,16 @@
 package ch.opentrainingcenter.otc.training.events;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import ch.opentrainingcenter.otc.training.domain.Athlete;
 import ch.opentrainingcenter.otc.training.domain.raw.Training;
 import ch.opentrainingcenter.otc.training.repository.AthleteRepository;
 import ch.opentrainingcenter.otc.training.repository.TrainingRepository;
@@ -21,6 +26,9 @@ class TrainingListenerTest {
 	@Mock
 	private Training training;
 
+	@Mock
+	private Athlete athlete;
+
 	@BeforeEach
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
@@ -28,11 +36,21 @@ class TrainingListenerTest {
 	}
 
 	@Test
-	void test() {
+	void testHappyCase() {
 		listener.onAddTraining(training);
-		Mockito.verify(athleteRepo).doSave(Mockito.any());
-		Mockito.verify(training).setAthlete(Mockito.any());
-		Mockito.verify(trainingRepo).doSave(training);
+		verify(athleteRepo).doSave(any());
+		verify(training).setAthlete(any());
+		verify(trainingRepo).doSave(training);
+	}
+
+	@Test
+	void testAthleteIsNull() {
+		when(athleteRepo.findByEmail(any())).thenReturn(athlete);
+		listener.onAddTraining(training);
+		verify(athleteRepo).findByEmail(any());
+		verifyNoMoreInteractions(athleteRepo);
+		verify(training).setAthlete(any());
+		verify(trainingRepo).doSave(training);
 	}
 
 }
