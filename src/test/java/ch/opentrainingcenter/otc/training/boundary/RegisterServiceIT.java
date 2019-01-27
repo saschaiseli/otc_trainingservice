4e982dbff08215d6613c5e5698e46b176fd137ab
sascha iseli <sascha.iseli@gmx.ch>
2019-01-27 20:44:45 +0100

    add IT test

--- src/test/java/ch/opentrainingcenter/otc/training/boundary/RegisterServiceIT.java
+++ src/test/java/ch/opentrainingcenter/otc/training/boundary/RegisterServiceIT.java
@@ -4,33 +4,38 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Response;
-import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.startsWith;
 
 public class RegisterServiceIT {
-    private final static Random random = new Random();
+    private final static String PATH = "http://localhost:8282";
 
     @Test
     public void shouldRegisterUser() {
         // register
         final String username = generateUsername();
         final String password = "secret";
+        doCreateUser(username, password);
+        doLogin(username, password);
+    }
 
-        given().baseUri("http://localhost:8282").when()//
+    private static void doCreateUser(final String username, final String password) {
+        given().baseUri(PATH).when()//
                 .body(createJSON(username, password))//
                 .contentType("application/json")//
                 .post("/trainingservice/api/users/register")//
                 .then().statusCode(Response.Status.OK.getStatusCode());
-//                .header("Baerer", is(""));
+    }
 
-        given().baseUri("http://localhost:8282").when()//
+    private static String doLogin(final String username, final String password) {
+        final io.restassured.response.Response response = given().baseUri(PATH).when()//
                 .body("{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}")//
                 .contentType("application/json")//
                 .post("/trainingservice/api/users/authenticate").then()//
                 .statusCode(Response.Status.OK.getStatusCode())//
-                .header("Authorization", startsWith("Bearer"));
+                .header("Authorization", startsWith("Bearer")).extract().response();
+        return response.getHeader("Authorization");
     }
 
     private static String createJSON(final String username, final String password) {
