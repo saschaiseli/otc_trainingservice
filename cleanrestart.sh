d705e903cb6f2b610127d439195545198d4542bd
sascha iseli <sascha.iseli@gmx.ch>
2019-05-24 22:29:31 +0200

    upgrade jars

--- cleanrestart.sh
+++ cleanrestart.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-docker stop trainingservice || true && docker rm trainingservice || true
-export FLYWAY_URL=jdbc:postgresql://$DB_HOST_PROD:$DB_PORT_PROD/$DB_DATABASE_PROD
-export FLYWAY_DRIVER=org.postgresql.Driver
-export FLYWAY_USER=$DB_USER_PROD
-export FLYWAY_PASSWORD=$DB_PASS_PROD
+if [ $# -lt 1 ]
+then
+    echo "Missing Parameter for Service Version (now using latest)"
+    TAG='latest'
+else
+    TAG=$1
+fi
+
+docker stop trainingservice || true
+docker rm -f trainingservice || true
+export FLYWAY_URL=jdbc:postgresql://$DB_HOST_PROD:$DB_PORT_PROD/$DB_DATABASE_PROD &
+export FLYWAY_DRIVER=org.postgresql.Driver &
+export FLYWAY_USER=$DB_USER_PROD &
+export FLYWAY_PASSWORD=$DB_PASS_PROD &
 docker run -it --rm --net=host \
     -e DB_USERNAME=$DB_USER_PROD \
     -e DB_PASSWORD=$DB_PASS_PROD \
@@ -12,4 +21,4 @@ docker run -it --rm --net=host \
     -e DB_PORT=$DB_PORT_PROD \
     -e DB_DATABASE=trainingservice \
     --name trainingservice \
-    iselisa/trainingservice:$1
+    iselisa/trainingservice:$TAG
